8be24af4a7d2c6d814c7d8f5e5f1815713b8ddd3
Chukwuemeka Maduekwe <maduekwepedro@gmail.com>
2024-12-30 09:07:07 +0100

    add home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,25 @@
-// import dynamic from "next/dynamic";
+import dynamic from "next/dynamic";
 import type { Metadata } from "next";
 
-// const ApiHub = dynamic(() => import("components/home/ApiHub")),
-//   WelcomeScreen = dynamic(() => import("components/home/Welcome")),
-//   RefreshHome = dynamic(() => import("components/home/RefreshHome")),
-//   FooterContainer = dynamic(() => import("components/layouts/footer")),
-//   Header = dynamic(() => import("components/layouts/header/HeaderContainer")),
-//   ManagerContainer = dynamic(() => import("components/home/ManagerContainer")),
+const ApiHub = dynamic(() => import("components/home/ApiHub")),
+  WelcomeScreen = dynamic(() => import("components/home/Welcome")),
+  // RefreshHome = dynamic(() => import("components/home/RefreshHome")),
+  FooterContainer = dynamic(() => import("components/layouts/footer")),
+  Header = dynamic(() => import("components/layouts/header/HeaderContainer")),
+  ManagerContainer = dynamic(() => import("components/home/ManagerContainer"));
 
 export const metadata: Metadata = { title: "Soccer Manager", description: "Revamped Football Manager" };
 
 const HomePage = () => (
   <>
-    {/* <Header position="relative" />
+    <Header position="relative" />
     <main>
       <WelcomeScreen />
       <ManagerContainer />
       <ApiHub />
-    </main> */}
-    {/* <FooterContainer /> */}
+    </main>
+    <FooterContainer />
     {/* <RefreshHome /> */}
-    <div>page</div>;
   </>
 );
 
